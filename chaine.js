5d868f59bcaaf36bc42b7f62fe6b0e3a5965dcea
NAJEEHAT APEKE WAHAB <wahabnanjeehat@gmail.com>
2024-05-21 10:54:07 +0000

    Update chaine.js

--- chaine.js
+++ chaine.js
@@ -1,6 +1,6 @@
 function reverseString(str) {
     let reversed = "";
-    for (let) i = str.length - 1; i >= 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
       reversed += str[i];
     }
     return reversed;
